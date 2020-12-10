79a9b1d409ae208c099d58af234f96a95069a3e9
Jordan Heintz <heintz.jordan.dakota@gmail.com>
2020-12-10 14:57:29 -0500

    changed alert in first exercise to console.log

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ const occupation = prompt(`Hi, ${name}! What do you do?`.toUpperCase());
 const love = prompt(`Cool! Do you love being a ${occupation}?`);
 
 if(love === "yes" || "Yes"){
-    alert(`${name} is a ${occupation} and loves it!`);
+    console.log(`${name} is a ${occupation} and loves it!`);
 }else{
-    alert(`${name} is a ${occupation} and does not love it.`);
+    console.log(`${name} is a ${occupation} and does not love it.`);
 }
