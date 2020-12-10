477591072f8df6de7bf8cb992648ead9797240ee
Jordan Heintz <heintz.jordan.dakota@gmail.com>
2020-12-10 15:18:07 -0500

    finished exercise two, moved script tag after closing body tag

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+// Challenge 1
+
 const name = prompt("What is your name?");
 const occupation = prompt(`Hi, ${name}! What do you do?`.toUpperCase());
 const love = prompt(`Cool! Do you love being a ${occupation}?`);
@@ -7,3 +9,12 @@ if(love === "yes" || "Yes"){
 }else{
     console.log(`${name} is a ${occupation} and does not love it.`);
 }
+
+//Challenge 2
+
+const colors = ["red", "green", "blue"];
+const nameTwo = prompt("Hi! What is your name?");
+const colorChoice = prompt(`${nameTwo}, choose a color: ${colors}`);
+const colorRespone = alert(`You chose ${colorChoice}`);
+const colorBroadcast = `<p>${nameTwo} chose ${colorChoice}!!!</p>`;
+document.querySelector(`main`).innerHTML = colorBroadcast;
